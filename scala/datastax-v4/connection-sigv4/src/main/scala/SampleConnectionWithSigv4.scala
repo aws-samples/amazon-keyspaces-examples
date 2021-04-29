6031e421a021424699674df0310520e4ca5ca0d3
Brian Houser <brianh@amazon.com>
2021-04-29 11:22:16 -0700

    Introducing Scala SigV4 Connection Example
    Introduces a Scala Example for SigV4 and DataStax V4 driver.

---
+++ scala/datastax-v4/connection-sigv4/src/main/scala/SampleConnectionWithSigv4.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ */
+import com.datastax.oss.driver.api.core.CqlSession
+
+import scala.jdk.CollectionConverters._
+
+object SampleConnectionWithSigv4 {
+
+  // This code uses the configuration present in ./resources/application.conf
+  // and the AWS default credential chain.
+  // see https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html.
+  def main(args: Array[String]): Unit = {
+    val resultSet = session.execute("select * from system_schema.keyspaces");
+    val rows = resultSet.all().asScala;
+    rows.foreach({ println } );
+
+    println("List of all Keyspaces in this region...");
+    for (row <- rows) println(row.getString("keyspace_name"));
+
+    System.exit(0);
+  }
+
+  private val session = CqlSession.builder.build()
+}
