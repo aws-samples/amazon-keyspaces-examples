079dc880c4f4cee38bf5bb47dfe44037cb64f769
Sri Rathan Rangisetti <srirathan.vlit@gmail.com>
2022-03-31 09:47:31 -0700

    python cassadra driver retry policy (#20)

--- python/datastax-v3/connection-sigv4/sigv4_sample/connectkeyspaces.py
+++ python/datastax-v3/connection-sigv4/sigv4_sample/connectkeyspaces.py
@@ -10,9 +10,11 @@ try:
     from boto3 import Session
     from cassandra_sigv4.auth import AuthProvider, Authenticator, SigV4AuthProvider
     from ssl import SSLContext, PROTOCOL_TLSv1_2, CERT_REQUIRED
-    from cassandra.cluster import Cluster
+    from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
     from cassandra import ConsistencyLevel
     from cassandra.query import SimpleStatement
+    from KeyspacesRetryPolicy import KeyspacesRetryPolicy
+
 except ImportError:
     raise RuntimeError('Required packages Failed To install please run "python Setup.py install" command or install '
                        'using pip')
@@ -35,7 +37,6 @@ def main():
         if not credentials or not credentials.access_key:
             sys.exit("No access key found, please setup credentials (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY) according to https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html#cli-configure-quickstart-precedence\n")
     
-        
         region = boto_session.region_name
 
         if not region:  
@@ -45,10 +46,15 @@ def main():
         auth_provider = SigV4AuthProvider(boto_session)
         contact_point = "cassandra.{}.amazonaws.com".format(region)
 
+        profile = ExecutionProfile(
+            consistency_level=ConsistencyLevel.LOCAL_QUORUM,
+            retry_policy=KeyspacesRetryPolicy(RETRY_MAX_ATTEMPTS=5))
+
         cluster = Cluster([contact_point], 
                          ssl_context=ssl_context, 
                          auth_provider=auth_provider,
-                         port=9142)
+                         port=9142,
+                         execution_profiles={EXEC_PROFILE_DEFAULT: profile})
 
         session = cluster.connect()
         print("CONNECTION TO KEYSPACES SUCCESSFUL2")
