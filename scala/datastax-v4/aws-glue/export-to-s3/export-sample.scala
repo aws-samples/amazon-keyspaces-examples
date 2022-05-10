8251ed2c592af52f1fe213a3e1f0d18700c6ce70
michaelraney <raneymike83@gmail.com>
2022-05-09 21:42:47 -0400

    Export to S3
    Export to S3 using AWS Glue

---
+++ scala/datastax-v4/aws-glue/export-to-s3/export-sample.scala
@@ -0,0 +1,48 @@
+import com.amazonaws.services.glue.GlueContext
+import com.amazonaws.services.glue.util.GlueArgParser
+import com.amazonaws.services.glue.util.Job
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.from_json
+import org.apache.spark.sql.streaming.Trigger
+import scala.collection.JavaConverters._
+import com.datastax.spark.connector._
+import org.apache.spark.sql.cassandra._
+import org.apache.spark.sql.SaveMode._
+
+
+
+object GlueApp {
+
+  def main(sysArgs: Array[String]) {
+
+    val spark: SparkContext = new SparkContext()
+    val glueContext: GlueContext = new GlueContext(spark)
+    val sparkSession: SparkSession = glueContext.getSparkSession
+
+    import com.datastax.spark.connector._
+    import org.apache.spark.sql.cassandra._
+    import sparkSession.implicits._
+
+    // @params: [JOB_NAME]
+    val args = GlueArgParser.getResolvedOptions(sysArgs, Seq("JOB_NAME", "KEYSPACE_NAME", "TABLE_NAME", "S3_URI", "FORMAT").toArray)
+    Job.init(args("JOB_NAME"), glueContext, args.asJava)
+
+    val tableName = args("TABLE_NAME")
+    val keyspaceName = args("KEYSPACE_NAME")
+    val backupS3 = args("S3_URI")
+    val backupFormat = args("FORMAT")
+
+    val tableDf = sparkSession.read
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map( "table" -> tableName, "keyspace" -> keyspaceName))
+      .load()
+
+    tableDf.write.format(backupFormat).mode(SaveMode.Overwrite).save(backupS3)
+
+    Job.commit()
+  }
+}
